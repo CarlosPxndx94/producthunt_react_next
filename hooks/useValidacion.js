665ba4c50623aa1278ff9538fa4fb1a3d81121da
CarlosBalcazar <cbalcazar8@misena.edu.co>
2020-08-10 23:51:36 -0500

    Se inicia creación de Hook personalizado para realizar validaciones a los formularios

---
+++ hooks/useValidacion.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+
+const useValidacion = (stateInicial, validar, fn) => {
+
+    const [values, setValues] = useState(stateInicial);
+    const [errores, setErrors] = useState({});
+    const [submitForm, setSubmitForm] = useState(false);
+
+    useEffect(() => {
+        if (submitForm) {
+            const noErrors = Object.keys(errores).length === 0;
+
+            if (noErrors) {
+                fn(); //fn => Funcion que se ejecuta en el componente
+            }
+
+            setSubmitForm(false);
+        }
+    }, []);
+
+    //Funcion que se ejecuta mientras el usuario escribe.
+    const handleChange = e => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    //Funcion que se ejcuta cuando se hace submit
+    const handleSubmit = e => {
+        e.preventDefault();
+        const erroresValidacion = validar(values);
+        setErrors(erroresValidacion);
+        setSubmitForm(true);
+    }
+
+    return {
+        values,
+        errores,
+        submitForm,
+        handleChange,
+        handleSubmit
+    }
+}
+
+export default useValidacion;
--- pages/crear_cuenta.js
+++ pages/crear_cuenta.js
@@ -1,11 +1,80 @@
 import React from 'react'
 import Layout from '../components/layout/Layout'
+import {
+  Formulario,
+  Campo,
+  InputSubmit,
+  H1Form
+} from '../components/ui/Formulario'
+
+//validaciones hook
+import useValidacion from '../hooks/useValidacion'
+import validarCrearCuenta from '../validacion/validarCrearCuenta'
+
+const STATE_INICIAL = {
+  nombre: '',
+  email: '',
+  password: ''
+}
 
 export default function CrearCuenta() {
+
+  const {
+    values,
+    errores,
+    submitForm,
+    handleChange,
+    handleSubmit
+  } = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
+
+  //handle de crear cuenta
+  function crearCuenta() {
+    console.log("Creando Cuenta");
+  }
+
   return (
     <div>
       <Layout>
-        <h1>CrearCuenta</h1>
+        <>
+          <H1Form>Crear Cuenta</H1Form>
+          <Formulario>
+            <Campo>
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                type="text"
+                name="nombre"
+                id="nombre"
+                placeholder="Tu Nombre"
+              />
+            </Campo>
+
+            <Campo>
+              <label htmlFor="email">E-mail</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Tu E-mail"
+              />
+            </Campo>
+
+            <Campo>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Tu Password"
+              />
+            </Campo>
+
+            <InputSubmit
+              type="submit"
+              value="Crear Cuenta"
+            />
+
+          </Formulario>
+        </>
       </Layout>
     </div>
   )
